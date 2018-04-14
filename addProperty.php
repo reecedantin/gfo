4d4458e5a71d94d712c8a2aed6049a77565f024f
Kunal Sharma <kunalsharma.usa@gmail.com>
2018-04-14 16:14:50 -0400

    front end changes

--- addProperty.php
+++ addProperty.php
@@ -17,128 +17,107 @@
         </div> <!-- End Column --> 
       </div> <!-- End Row --> 
 
-      <div class="row manage-card text-center">
+      <div class="row">
         <div class="col-md-12">
 
           <form class="form-horizontal">
             <fieldset>
 
-
-
-              <label class="col-md-12 control-label" for="propertyName">Name</label>
-              <div class="col-md-12">
-                <input id="propertyName" name="propertyName" type="text" placeholder="Property Name" class="form-control input-md" required="">
-              </div> <br> 
-
-              <label class="col-md-12 control-label" for="address">Address</label>  
-              <div class="col-md-12">
-                <input id="address" name="address" type="text" placeholder="Address" class="form-control input-md" required="">
-              </div> <br> 
-
-              <label class="col-md-12 control-label" for="city">City</label>  
-              <div class="col-md-12">
-                <input id="city" name="city" type="text" placeholder="City" class="form-control input-md" required="">
-              </div> <br> 
-
-
-              <label class="col-md-12 control-label" for="zip">Zip</label>  
-              <div class="col-md-12">
-                <input id="zip" name="zip" type="text" placeholder="Zip" class="form-control input-md" required="">
-              </div> <br> 
-
-
-              <label class="col-md-12 control-label" for="size">Size (acres)</label>  
-              <div class="col-md-12">
-                <input id="size" name="size" type="text" placeholder="Size" class="form-control input-md" required="">
-              </div> <br> 
-
-              <label class="col-md-12 control-label" for="type">Type</label>  
-              <div class="col-md-12">
-                <input id="type" name="type" type="text" placeholder="Type" class="form-control input-md" value="Garden" disabled>
-              </div> <br> 
-
-
-              <label class="col-md-12 control-label" for="public">Public</label>
-              <div class="col-md-12">
-                <select id="public" name="public" class="form-control">
-                  <option value="1">True</option>
-                  <option value="2">Flase</option>
-                </select>
-              </div> <br> 
-
-              <label class="col-md-12 control-label" for="public">Commercial</label>
-              <div class="col-md-12">
-                <select id="public" name="public" class="form-control">
-                  <option value="1">True</option>
-                  <option value="2">Flase</option>
-                </select>
-              </div> <br> 
-
-              <label class="col-md-12 control-label" for="type">ID</label>  
-              <div class="col-md-12">
-                <input id="type" name="type" type="text" placeholder="Type" class="form-control input-md" value="00122" disabled>
-              </div> <br> 
-
-              <!-- Multiple Checkboxes -->
-              <div class="form-group">
-                <label class="col-md-12 control-label" for="currentCrops">Current Crops</label>
+              <div class="row manage-card text-center">
                 <div class="col-md-12">
-                  <div class="checkbox">
-                    <label for="currentCrops-0">
-                      <input type="checkbox" name="currentCrops" id="currentCrops-0" value="1" checked="checked">
-                      Wheat
-                    </label>
-                  </div>
-                  <div class="checkbox">
-                    <label for="currentCrops-1">
-                      <input type="checkbox" name="currentCrops" id="currentCrops-1" value="2" checked="checked">
-                      Corn
-                    </label>
-                  </div>
-                </div>
-              </div>
 
-              <div class="form-group">
-                <label class="col-md-12 control-label" for="addCrop">Add New Crop</label>
+                  <label class="col-md-12 control-label" for="propertyName">Property Name*</label>
+                  <div class="col-md-12">
+                    <input id="propertyName" name="propertyName" type="text" placeholder="Property Name" class="form-control input-md" required="">
+                  </div> <br> 
 
-                <div class="col-md-12 rowspace">
-                  <select id="public" name="public" class="form-control">
-                    <option value="1">Coffee</option>
-                    <option value="2">Spinach</option>
-                  </select>
-                </div> 
-                <div class="col-md-12">
-                  <button id="addCrop" name="addCrop" class="btn btn-primary style-bkg" style="width: 100%;">Add Crop to Property</button>
-                </div>
-              </div> <!-- End form group -->
+                  <label class="col-md-12 control-label" for="address">Street Address*</label>  
+                  <div class="col-md-12">
+                    <input id="streetAddress" name="streetAddress" type="text" placeholder="Street Address" class="form-control input-md" required="">
+                  </div> <br> 
 
-              <div class="form-group">
-                <label class="col-md-12 control-label" for="addCrop">Request Crop Approval</label>
+                  <label class="col-md-12 control-label" for="city">City*</label>  
+                  <div class="col-md-12">
+                    <input id="city" name="city" type="text" placeholder="City" class="form-control input-md" required="">
+                  </div> <br> 
 
-                <div class="col-md-12">
-                  <input id="cropRequest" name="cropRequest" type="text" placeholder="Enter New Crop Name" class="form-control input-md">
+
+                  <label class="col-md-12 control-label" for="zip">Zip*</label>  
+                  <div class="col-md-12">
+                    <input id="zip" name="zip" type="text" placeholder="Zip" class="form-control input-md" required="">
+                  </div> <br> 
+
+
+                  <label class="col-md-12 control-label" for="size">Size* (acres)</label>  
+                  <div class="col-md-12">
+                    <input id="size" name="size" type="text" placeholder="Size" class="form-control input-md" required="">
+                  </div> <br> 
+
+                  <label class="col-md-12 control-label" for="type">Type*</label>
+                  <div class="col-md-12">
+                    <select id="type" name="type" class="form-control">
+                      <option value="1">Farm</option>
+                      <option value="2">Garden</option>
+                    </select>
+                  </div> <br> 
+
+
+                  <label class="col-md-12 control-label" for="animal">Animal*</label>
+                  <div class="col-md-12">
+                    <select id="animal" name="animal" class="form-control">
+                      <option value="1">Snake</option>
+                      <option value="2">Lizard</option>
+                    </select>
+                  </div> <br> 
+
+                  <label class="col-md-12 control-label" for="crop">Crop*</label>
+                  <div class="col-md-12">
+                    <select id="crop" name="crop" class="form-control">
+                      <option value="1">Wheat</option>
+                      <option value="2">Corn</option>
+                    </select>
+                  </div> <br> 
+
+
+                  <label class="col-md-12 control-label" for="public">Public?*</label>
+                  <div class="col-md-12">
+                    <select id="public" name="public" class="form-control">
+                      <option value="1">True</option>
+                      <option value="2">Flase</option>
+                    </select>
+                  </div> <br> 
+
+                  <label class="col-md-12 control-label" for="commercial">Commercial*</label>
+                  <div class="col-md-12">
+                    <select id="commercial" name="commercial" class="form-control">
+                      <option value="1">True</option>
+                      <option value="2">Flase</option>
+                    </select>
+                  </div> <br> 
+
+                </div> <!-- End Column -->
+              </div> <!-- End Row -->
+
+
+              <!-- Button Bar -->
+              <div class="row button-adjust">
+                <div class="col-md-6">
+                  <button i="addCrop" name="addCrop" class="btn btn-success style-bkg" style="width: 100%;">&#x2b Add Property</button>
                 </div>
 
-                <div class="col-md-12">
-                  <button id="addCrop" name="addCrop" class="btn btn-primary style-bkg" style="width: 100%;">Submit Request</button>
+                <div class="col-md-6">
+                  <button id="addCrop" name="addCrop" class="btn btn-secondary" style="width: 100%;">&#x2190 Cancel</button>
                 </div>
-              </div> <!-- End form group -->
+              </div> <!-- End Row -->
+
 
             </fieldset>
           </form>
 
-        </div> <!-- End Column -->
-      </div> <!-- End Row -->
+        </div> <!-- End Outer Column -->
+      </div> <!-- End Outer Row --> 
 
-      <div class="row button-adjust">
-        <div class="col-md-6">
-          <button i="addCrop" name="addCrop" class="btn btn-success style-bkg" style="width: 100%;">&#x2b Add Property</button>
-        </div>
 
-        <div class="col-md-6">
-          <button id="addCrop" name="addCrop" class="btn btn-secondary" style="width: 100%;">&#x2190 Cancel</button>
-        </div>
-      </div> <!-- End Row -->
 
     </div> <!-- End Container -->
   </section>
--- manageProperty.php
+++ manageProperty.php
@@ -17,132 +17,142 @@
         </div> <!-- End Column --> 
       </div> <!-- End Row --> 
 
-      <div class="row manage-card text-center">
+
+      <div class="row">
         <div class="col-md-12">
 
-          <form class="form-horizontal">
-            <fieldset>
+
+          <div class="row manage-card text-center">
+            <div class="col-md-12">
+
+              <form class="form-horizontal">
+                <fieldset>
 
 
+                  <label class="col-md-12 control-label" for="propertyName">Property Name*</label>
+                  <div class="col-md-12">
+                    <input id="propertyName" name="propertyName" type="text" placeholder="Property Name" class="form-control input-md" required="">
+                  </div> <br> 
 
-              <label class="col-md-12 control-label" for="propertyName">Name</label>
-              <div class="col-md-12">
-                <input id="propertyName" name="propertyName" type="text" placeholder="Property Name" class="form-control input-md" required="">
-              </div> <br> 
+                  <label class="col-md-12 control-label" for="address">Address*</label>  
+                  <div class="col-md-12">
+                    <input id="address" name="address" type="text" placeholder="Address" class="form-control input-md" required="">
+                  </div> <br> 
 
-              <label class="col-md-12 control-label" for="address">Address</label>  
-              <div class="col-md-12">
-                <input id="address" name="address" type="text" placeholder="Address" class="form-control input-md" required="">
-              </div> <br> 
-
-              <label class="col-md-12 control-label" for="city">City</label>  
-              <div class="col-md-12">
-                <input id="city" name="city" type="text" placeholder="City" class="form-control input-md" required="">
-              </div> <br> 
-
-
-              <label class="col-md-12 control-label" for="zip">Zip</label>  
-              <div class="col-md-12">
-                <input id="zip" name="zip" type="text" placeholder="Zip" class="form-control input-md" required="">
-              </div> <br> 
+                  <label class="col-md-12 control-label" for="city">City*</label>  
+                  <div class="col-md-12">
+                    <input id="city" name="city" type="text" placeholder="City" class="form-control input-md" required="">
+                  </div> <br> 
 
 
-              <label class="col-md-12 control-label" for="size">Size (acres)</label>  
-              <div class="col-md-12">
-                <input id="size" name="size" type="text" placeholder="Size" class="form-control input-md" required="">
-              </div> <br> 
-
-              <label class="col-md-12 control-label" for="type">Type</label>  
-              <div class="col-md-12">
-                <input id="type" name="type" type="text" placeholder="Type" class="form-control input-md" value="Garden" disabled>
-              </div> <br> 
+                  <label class="col-md-12 control-label" for="zip">Zip*</label>  
+                  <div class="col-md-12">
+                    <input id="zip" name="zip" type="text" placeholder="Zip" class="form-control input-md" required="">
+                  </div> <br> 
 
 
-              <label class="col-md-12 control-label" for="public">Public</label>
-              <div class="col-md-12">
-                <select id="public" name="public" class="form-control">
-                  <option value="1">True</option>
-                  <option value="2">Flase</option>
-                </select>
-              </div> <br> 
+                  <label class="col-md-12 control-label" for="size">Size* (acres)</label>  
+                  <div class="col-md-12">
+                    <input id="size" name="size" type="text" placeholder="Size" class="form-control input-md" required="">
+                  </div> <br> 
 
-              <label class="col-md-12 control-label" for="public">Commercial</label>
-              <div class="col-md-12">
-                <select id="public" name="public" class="form-control">
-                  <option value="1">True</option>
-                  <option value="2">Flase</option>
-                </select>
-              </div> <br> 
+                  <label class="col-md-12 control-label" for="type">Type*</label>  
+                  <div class="col-md-12">
+                    <input id="type" name="type" type="text" placeholder="Type" class="form-control input-md" value="Garden" disabled>
+                  </div> <br> 
 
-              <label class="col-md-12 control-label" for="type">ID</label>  
-              <div class="col-md-12">
-                <input id="type" name="type" type="text" placeholder="Type" class="form-control input-md" value="00122" disabled>
-              </div> <br> 
 
-              <!-- Multiple Checkboxes -->
-              <div class="form-group">
-                <label class="col-md-12 control-label" for="currentCrops">Current Crops</label>
-                <div class="col-md-12">
-                  <div class="checkbox">
-                    <label for="currentCrops-0">
-                      <input type="checkbox" name="currentCrops" id="currentCrops-0" value="1" checked="checked">
-                      Wheat
-                    </label>
+                  <label class="col-md-12 control-label" for="public">Public*</label>
+                  <div class="col-md-12">
+                    <select id="public" name="public" class="form-control">
+                      <option value="1">True</option>
+                      <option value="2">Flase</option>
+                    </select>
+                  </div> <br> 
+
+                  <label class="col-md-12 control-label" for="public">Commercial*</label>
+                  <div class="col-md-12">
+                    <select id="public" name="public" class="form-control">
+                      <option value="1">True</option>
+                      <option value="2">Flase</option>
+                    </select>
+                  </div> <br> 
+
+                  <label class="col-md-12 control-label" for="type">ID</label>  
+                  <div class="col-md-12">
+                    <input id="type" name="type" type="text" placeholder="Type" class="form-control input-md" value="00122" disabled>
+                  </div> <br> 
+
+                  <!-- Multiple Checkboxes -->
+                  <div class="form-group">
+                    <label class="col-md-12 control-label" for="currentCrops">Current Crops*</label>
+                    <div class="col-md-12">
+                      <div class="checkbox">
+                        <label for="currentCrops-0">
+                          <input type="checkbox" name="currentCrops" id="currentCrops-0" value="1" checked="checked">
+                          Wheat
+                        </label>
+                      </div>
+                      <div class="checkbox">
+                        <label for="currentCrops-1">
+                          <input type="checkbox" name="currentCrops" id="currentCrops-1" value="2" checked="checked">
+                          Corn
+                        </label>
+                      </div>
+                    </div>
                   </div>
-                  <div class="checkbox">
-                    <label for="currentCrops-1">
-                      <input type="checkbox" name="currentCrops" id="currentCrops-1" value="2" checked="checked">
-                      Corn
-                    </label>
-                  </div>
+
+                  <div class="form-group">
+                    <label class="col-md-12 control-label" for="addCrop">Add New Crop</label>
+
+                    <div class="col-md-12 rowspace">
+                      <select id="public" name="public" class="form-control">
+                        <option value="1">Coffee</option>
+                        <option value="2">Spinach</option>
+                      </select>
+                    </div> 
+                    <div class="col-md-12">
+                      <button id="addCrop" name="addCrop" class="btn btn-primary style-bkg" style="width: 100%;">Add Crop to Property</button>
+                    </div>
+                  </div> <!-- End form group --> <br>
+
+                  <div class="form-group">
+                    <label class="col-md-12 control-label" for="addCrop">Request Crop Approval</label>
+
+                    <div class="col-md-12 rowspace">
+                      <input id="cropRequest" name="cropRequest" type="text" placeholder="Enter New Crop Name" class="form-control input-md">
+                    </div>
+
+                    <div class="col-md-12">
+                      <button id="addCrop" name="addCrop" class="btn btn-primary style-bkg" style="width: 100%;">Submit Request</button>
+                    </div>
+                  </div> <!-- End form group -->
+                  <br>
+
+
+                </div> <!-- End Column -->
+              </div> <!-- End Row -->
+
+
+              <div class="row button-adjust">
+                <div class="col-md-4">
+                  <button id="addCrop" name="addCrop" class="btn btn-success style-bkg" style="width: 100%;">&#x1f4be Save</button>
                 </div>
-              </div>
 
-              <div class="form-group">
-                <label class="col-md-12 control-label" for="addCrop">Add New Crop</label>
-
-                <div class="col-md-12 rowspace">
-                  <select id="public" name="public" class="form-control">
-                    <option value="1">Coffee</option>
-                    <option value="2">Spinach</option>
-                  </select>
-                </div> 
-                <div class="col-md-12">
-                  <button id="addCrop" name="addCrop" class="btn btn-primary style-bkg" style="width: 100%;">Add Crop to Property</button>
-                </div>
-              </div> <!-- End form group -->
-
-              <div class="form-group">
-                <label class="col-md-12 control-label" for="addCrop">Request Crop Approval</label>
-
-                <div class="col-md-12">
-                  <input id="cropRequest" name="cropRequest" type="text" placeholder="Enter New Crop Name" class="form-control input-md">
+                <div class="col-md-4">
+                  <button  name="addCrop" class="btn btn-danger" style="width: 100%;">&#x2718 Delete</button>
                 </div>
 
-                <div class="col-md-12">
-                  <button id="addCrop" name="addCrop" class="btn btn-primary style-bkg" style="width: 100%;">Submit Request</button>
+                <div class="col-md-4">
+                  <a href="ownerDashboard.php"><button name="addCrop" class="btn btn-secondary" style="width: 100%;">&#x2190 Cancel</button></a>
                 </div>
-              </div> <!-- End form group -->
+              </div> <!-- End Row -->
 
             </fieldset>
           </form>
 
-        </div> <!-- End Column -->
-      </div> <!-- End Row -->
-
-      <div class="row button-adjust">
-        <div class="col-md-4">
-          <button id="addCrop" name="addCrop" class="btn btn-success style-bkg" style="width: 100%;">&#x1f4be Save</button>
-        </div>
-
-        <div class="col-md-4">
-          <button id="addCrop" name="addCrop" class="btn btn-danger" style="width: 100%;">&#x2718 Delete</button>
-        </div>
-
-        <div class="col-md-4">
-          <button id="addCrop" name="addCrop" class="btn btn-secondary" style="width: 100%;">&#x2190 Cancel</button>
-        </div>
-      </div> <!-- End Row -->
+        </div> <!-- End Outer Column -->
+      </div> <!-- End Outer Row --> 
 
     </div> <!-- End Container -->
   </section>
