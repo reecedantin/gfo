4ad2681949a600fa846c777e4e982c79c748649d
psharma75 <38143271+psharma75@users.noreply.github.com>
2018-04-20 17:43:16 -0400

    Update visitorDashboard.php

--- visitorDashboard.php
+++ visitorDashboard.php
@@ -63,7 +63,7 @@
       <div class = "col-md-12">
 
         <div class="property-table">
-          <table class="table table-striped table-sm">
+          <table class="table table-striped table-sm sortable">
               <thead class="thead-dark">
                 <tr>
                   <th scope="col">Name</th>
