79c8aa38478211352602b11c6798b70fcb4e8238
psharma75 <38143271+psharma75@users.noreply.github.com>
2018-04-20 17:49:16 -0400

    Update ownerList.php

--- ownerList.php
+++ ownerList.php
@@ -81,7 +81,7 @@
         <div class = "col-md-12">
 
           <div class="property-table table-scroll">
-            <table class="table table-striped table-sm">
+            <table class="table table-striped table-sm sortable">
               <thead class="thead-dark">
                 <tr>
                   <th scope="col">Delete Owner</th>
